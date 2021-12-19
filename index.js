9be56b137f88483a6586238db9484b0c94dda1f1
Yasin ilkalp Arabacı <yasinilkalp_24@hotmail.com>
2021-12-19 22:50:54 +0300

    create mongoose

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+const mongoose = require("mongoose");
+const url =
+  "mongodb+srv://yasin:yasin123@today-in-history.wske3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+mongoose.connection.on("open", () => {
+  console.log("MongoDB: Connected");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB: Error", err);
+});
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+const Schema = new mongoose.Schema({
+  dateText: { type: String, unique: false, required: true },
+  year: { type: Number, unique: false, required: true },
+  typeName: { type: String, unique: false, required: true },
+  text: { type: String, unique: true, required: true },
+});
 
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+const history = mongoose.model("history", Schema);
+
+app.get("/year/:year", function (req, res, next) {
+  console.log("history.js: get /year/:year");
+  history
+    .find({ year: req.params.year })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
